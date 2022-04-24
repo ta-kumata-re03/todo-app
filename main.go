8e88dd4d1dec9249af368e92444e0a52701b9829
pyama2000 <takahiko-yamashita@retty.me>
2022-04-24 13:48:51 +0900

    Add Todo struct

--- main.go
+++ main.go
@@ -14,14 +14,16 @@ import (
 	"github.com/labstack/echo"
 )
 
-type (
-	todos struct {
-		ID          string `json:"id"`
-		Title       string `json:"title"`
-		Detail      string `json:"detail"`
-		Expire_date string `json:"expire_date"`
-	}
-)
+type Todos struct {
+	Todos []Todo `json:"todos"`
+}
+
+type Todo struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Detail      string `json:"detail"`
+	Expire_date string `json:"expire_date"`
+}
 
 func main() {
 	// [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
@@ -65,28 +67,29 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		var result []todos
+		var result []Todo
 		for rows.Next() {
-			todo := todos{}
+			todo := Todo{}
 			if err := rows.Scan(&todo.ID, &todo.Title); err != nil {
 				log.Fatal(err)
 			}
 			result = append(result, todo)
 		}
-		return c.JSON(http.StatusOK, result)
+		todos := Todos{result}
+		return c.JSON(http.StatusOK, todos)
 	})
 	//Select detail
 	e.GET("/todos/:id", func(c echo.Context) error {
 		id, _ := strconv.Atoi(c.Param("id"))
 		row := db.QueryRow("SELECT * FROM todo WHERE id=?", id)
 
-		var result todos
-		if err := row.Scan(&result.ID, &result.Title, &result.Detail, &result.Expire_date); err != nil {
+		var todo Todo
+		if err := row.Scan(&todo.ID, &todo.Title, &todo.Detail, &todo.Expire_date); err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Println("id: ", result.ID, ", title: ", result.Title, ", detail: ", result.Detail, "expire_date: ", result.Expire_date)
-		return c.String(http.StatusOK, "List display")
+		fmt.Println("id: ", todo.ID, ", title: ", todo.Title, ", detail: ", todo.Detail, "expire_date: ", todo.Expire_date)
+		return c.JSON(http.StatusOK, todo)
 	})
 	//Insert
 	e.POST("/create", func(c echo.Context) error {
